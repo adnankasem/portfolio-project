87ed8584408e8a28fc005c2994b4c8d29486138c
adnankasem <81336925+adnankasem@users.noreply.github.com>
2021-05-28 19:28:25 -0700

    Start of Javascript

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ btn.onclick = function(){
     container.style.transform = "rotate(" + number + "deg)";
     number += Math.ceil(Math.random() * 1000);
 }
+
+const input = document.querySelector("textarea");
+const divOne = document.getElementById("one");
+const divTwo = document.getElementById("two");
+
+
+const addFood = (e) => {
+    if (e.key === 'Enter') {
+        divOne.innerHTML = input.value;
+        console.log('Enter Pressed')
+        input.addEventListener('keyup', addFoodTwo)
+    }
+}
+
+input.addEventListener('keyup', addFood)
+
+const addFoodTwo = (e) => {
+    if (e.key === 'Enter') {
+        divTwo.innerHTML = input.value;
+        console.log('Enter Pressed Two')
+    }
+}
+
